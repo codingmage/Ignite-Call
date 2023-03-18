75d5389a76830c49a637f7d62c51879fb6cb52c8
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-03-18 15:57:47 -0300

    add visual style, user form and start of api

---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,31 @@
+import { getCssText } from '@ignite-ui/react'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Preciso disso abaixo para o estilo fucionar no server side rendering e não parar de funcionar se o JS for desabilitado */}
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
