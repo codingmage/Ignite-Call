84158abdacbf7046af9ba0f0785a369e11b06f8a
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-04-10 16:46:06 -0300

    add calendar page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,67 @@
+import { Avatar, Heading, Text } from "@ignite-ui/react";
+import { Container, UserHeader } from "./styles";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { prisma } from "@/lib/prisma";
+import { ScheduleForm } from "./ScheduleForm/ConfirmStep";
+
+interface ScheduleProps {
+    user: {
+        name: string,
+        bio: string,
+        avatarUrl: string,
+    }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+
+    return (
+        <Container>
+            <UserHeader>
+                <Avatar src={user.avatarUrl} />
+                <Heading>{user.name}</Heading>
+                <Text>{user.bio}</Text>
+            </UserHeader>
+
+            <ScheduleForm />
+        </Container>
+    )
+}
+
+// We need to generate 1 static page per user. GetStaticPath tells Next the username(params) so it can create a static page for it during the build part
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        // By passing an empty array (no pages), it tells the program to only generate a static page when a user will access it, and not during the build
+        paths: [],
+        fallback: 'blocking',
+    }
+}
+
+// Add Static Page behavior
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+
+    const username = String(params?.username)
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username,
+        },
+    })
+
+    if (!user) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            user: {
+                name: user.name,
+                bio: user.bio,
+                avatarUrl: user.avatar_url,
+            },
+        },
+        revalidate: 60 * 60 * 24, // 1 day
+    }
+}
