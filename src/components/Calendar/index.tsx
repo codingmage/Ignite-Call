f06aa21f9183a4108edb796d39e4e9090e00ea32
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-04-15 21:38:02 -0300

    confirm page and month handler

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,24 +1,46 @@
 import { CaretLeft, CaretRight } from "phosphor-react";
 import { CalendarActions, CalendarBody, CalendarContainer, CalendarDay, CalendarHeader, CalendarTitle } from "./styles";
 import { getWeekDays } from "@/utils/get-week-days";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function Calendar() {
 
+    const [ currentDate, setCurrentDate ] = useState(() => {
+        return dayjs().set('date', 1)
+    })
+
+    function handlePreviousMonth() {
+        const previousMonthDate = currentDate.subtract(1, 'month')
+
+        setCurrentDate(previousMonthDate)
+    }
+
+    function handleNextMonth() {
+        const nextMonthDate = currentDate.add(1, 'month')
+
+        setCurrentDate(nextMonthDate)
+    }
+
     const shortWeekDays = getWeekDays({short: true})
+
+    const currentMonth = currentDate.format('MMMM')
+    
+    const currentYear = currentDate.format('YYYY')
 
     return (
         <CalendarContainer>
             <CalendarHeader>
                 <CalendarTitle>
-                    Abril <span>2023</span>
+                    {currentMonth} <span>{currentYear}</span>
                 </CalendarTitle>
 
                 <CalendarActions>
-                    <button>
-                        <CaretLeft />
+                    <button onClick={handlePreviousMonth} title="Previous month">
+                        <CaretLeft  />
                     </button>
-                    <button>
-                        <CaretRight />
+                    <button onClick={handleNextMonth} title="Next month">
+                        <CaretRight  />
                     </button>
                 </CalendarActions>
             </CalendarHeader>
--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,7 +1,78 @@
-import { CalendarStep } from "../CalendarStep";
+import { CalendarBlank, Clock } from "phosphor-react";
+import { ConfirmForm, FormActions, FormError, FormHeader } from "./styles";
+import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export function ScheduleForm() {
+const confirmFormSchema = z.object({
+    name: z.string().min(3, { message: "O nome precisa de no mínimo 3 caracteres." }),
+    email: z.string().email({ message: "Digite um e-mail válido." }),
+    observations: z.string().nullable(),
+
+})
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>
+
+export function ConfirmStep() {
+
+    const { register, handleSubmit, formState: { isSubmitting, errors} } = useForm<ConfirmFormData>({
+        resolver: zodResolver(confirmFormSchema)
+    })
+
+    function handleConfirmScheduling(data: ConfirmFormData) {
+        console.log(data)
+    }
+
     return (
-        <CalendarStep/>
+        <ConfirmForm as='form' onSubmit={handleSubmit(handleConfirmScheduling)} >
+            <FormHeader>
+                <Text>
+                    <CalendarBlank />
+                    15 de Abril de 2023
+                </Text>
+                <Text>
+                    <Clock />
+                    18:00h
+                </Text>
+            </FormHeader>
+
+            <label>
+                <Text size="sm">
+                    Nome completo
+                </Text>
+                <TextInput placeholder="Seu nome" {...register("name")} />
+                {errors.name && (
+                    <FormError size="sm">
+                        {errors.name.message}
+                    </FormError>
+                )}
+            </label>
+
+            <label >
+                <Text size="sm">
+                    Endereço de e-mail
+                </Text>
+                <TextInput type="email" placeholder="johndoe@example.com" {...register("email")}/>
+                { errors.email && (
+                    <FormError size="sm" >
+                        {errors.email.message}
+                    </FormError>
+                )
+                }
+            </label>
+
+            <label >
+                <Text size="sm" {...register("observations")}>
+                    Observações
+                </Text>
+                <TextArea />
+            </label>
+
+            <FormActions>
+                <Button type="button" variant="tertiary">Cancelar</Button>
+                <Button type="submit" disabled={isSubmitting}>Confirmar</Button>
+            </FormActions>
+        </ConfirmForm>
     )
 }
